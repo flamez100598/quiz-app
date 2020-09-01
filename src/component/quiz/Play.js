1f25c16495f796d7b1a583747708652bbcdcd7e1
phamquocdung <phamquocdung@luvina.net>
2020-09-01 10:38:23 +0700

    update code

--- src/component/quiz/Play.js
+++ src/component/quiz/Play.js
@@ -1,26 +1,89 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-
+import questions from '../../question.json';
+import isEmpty from '../../ultils/is-empty';
 class Play extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            questions,
+            currentQuestion: {},
+            nextQuestion: {},
+            previousQuestion: {},
+            answer: '',
+            numberOfQuestions: 0,
+            numberOfAnsweredQuestion: 0,
+            currentQuestionIndex: 0,
+            score: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            hints: 5,
+            fiftyFifty: 2,
+            usedFiftyfifty: false,
+            time: {}
+        }
+    }
+    componentDidMount() {
+        const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state;
+        this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
+    }
+    displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion, ) => {
+        let { currentQuestionIndex } = this.state;
+        if (!isEmpty(questions)) {
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestionIndex + 1];
+            previousQuestion = questions[currentQuestionIndex - 1];
+            const answer = currentQuestion.answer;
+            this.setState({
+                currentQuestion,
+                nextQuestion,
+                previousQuestion,
+                answer
+            })
+        }
     }
     render() {
+        const { currentQuestion } = this.state;
         return (
             <Fragment>
-            <Helmet><title>Quiz Instructions - Quiz App</title></Helmet>
-            <div className="questions">
-                <h5>Google was founded in what year?</h5>
-                <div className="options-container">
-                    <p className="option">1997</p>
-                    <p className="option">1997</p>
+                <Helmet><title>Quiz Instructions - Quiz App</title></Helmet>
+                <div className="questions">
+                    <h2>Quiz mode</h2>
+                    <div className="lifeline-container">
+                        <p>
+                            <span className="mdi mdi-set-center mdi-24px lifeline-icon"></span><span className="lifeline">2</span>
+                        </p>
+                        <p>
+                            <span className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span><span className="lifeline">5</span>
+                        </p>
+                    </div>
+                    <div className="lifeline-container">
+                        <p>
+                            <span>1 of 15</span>
+
+                        </p>
+                        <p>
+                            <span className="mdi mdi-clock-outline mdi-24px"></span> 2:22
+                        </p>
+                    </div>
+                    <h5>{currentQuestion.question}</h5>
+                    <div className="options-container">
+                        <p className="option">{currentQuestion.optionA}</p>
+                        <p className="option">{currentQuestion.optionB}</p>
+                    </div>
+                    <div className="options-container">
+                        <p className="option">{currentQuestion.optionC}</p>
+                        <p className="option">{currentQuestion.optionD}</p>
+                    </div>
+                    <div className="button-container">
+                        <button>Previous</button>
+                        <button>Next</button>
+                        <button>Quit</button>
+                    </div>
                 </div>
-                <div className="options-container">
-                    <p className="option">1999</p>
-                    <p className="option">2000 </p>
-                </div>
-            </div>
+
             </Fragment>
         );
     }
